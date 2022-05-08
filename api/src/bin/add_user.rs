92fb83ddb6ce91636e90114820321de61696af72
Luke Pfeiffer <lpfeiff10@gmail.com>
2022-05-08 16:51:37 -0700

    Refactored the api project to use multiple binaries instead of a workspace

--- api/src/bin/add_user.rs
+++ api/src/bin/add_user.rs
@@ -1,16 +1,14 @@
-use lambda_http::{service_fn, Error, IntoResponse, Request, Body};
-use sqlx::PgPool;
+use api::{bad_request_response, created_response};
 use data_access::user::Users;
 use endurance_racing_planner_common::User;
-use utilities::{bad_request_response, created_response};
+use lambda_http::{service_fn, Body, Error, IntoResponse, Request};
+use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let db_context = data_access::initialize().await?;
     let db_context_ref = &db_context;
-    let handler = move |event: Request| {
-        add_user(event, db_context_ref)
-    };
+    let handler = move |event: Request| add_user(event, db_context_ref);
 
     lambda_http::run(service_fn(handler)).await?;
     Ok(())
@@ -25,13 +23,10 @@ async fn add_user(event: Request, db_context_ref: &PgPool) -> Result<impl IntoRe
                 Ok(u) => {
                     let new_user = Users::create_user(db_context_ref, u).await?;
                     created_response(&new_user, format!("/users/{}", new_user.id))
-                },
-                Err(e) => {
-                    bad_request_response(e.to_string())
                 }
+                Err(e) => bad_request_response(e.to_string()),
             }
-
-        },
+        }
         _ => bad_request_response("Invalid body type".into()),
     })
 }
